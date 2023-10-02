573d8e47ea9b74401102854cdd438e87c3dee92f
AhmadJanuar1 <ahmadjanuaramri2015@gmail.com>
2023-10-02 17:00:16 +0700

    portfolio with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#393E46",
+        dark: "#000000",
+        secondary: "#9DB2BF",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
